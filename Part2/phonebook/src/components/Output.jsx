98fa56a44062309fc2efc135b408ad61bd0a1ee7
Kevin C. Tofel <kevin@kctofel.com>
2023-09-21 10:18:48 -0400

    Prepping to move axios features to separate module

--- Part2/phonebook/src/components/Output.jsx
+++ Part2/phonebook/src/components/Output.jsx
@@ -2,7 +2,7 @@
 import Name from "./Name";
 
 const Output = (props) => {
-  console.log("Output got ", props);
+  // console.log("Output got ", props);
   return (
     <Name
       key={props.props.name}
